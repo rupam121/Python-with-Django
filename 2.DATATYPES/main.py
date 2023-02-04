d9d842dc04265424755406db0d51aaba071e0b09
RUPAM GIRI <103890234+rupam121@users.noreply.github.com>
2023-02-04 14:36:24 +0530

    Add files via upload

---
+++ 2.DATATYPES/main.py
@@ -0,0 +1,25 @@
+val1 = int(input("Enter val1: "))
+val2 = int(input("Enter val2: "))
+isGreater = val1 > val2
+print(isGreater, type(isGreater))
+# print(val1)
+# val1 += 1
+# print(val1)
+# val1 -= 1
+# print(val1)
+
+
+# print(val1+val2)
+# print(val1-val2)
+# print(val1*val2)
+# print(val1/val2)
+# print(val1//val2)
+# print(val2 % val1)
+# # print(val1 % val2)
+# print(val1**val2)  # exponential operater 5^2:25
+
+# print(val1 == val2, val1 is val2)
+# print(val1 > val2)
+# print(val1 < val2)
+# print(val1 >= val2)
+# print(val1 <= val2)
