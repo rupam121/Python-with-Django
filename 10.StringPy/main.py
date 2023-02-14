a562c3493ca5cbde61d4a4e3b0872e05d21bfeaf
RUPAM GIRI <103890234+rupam121@users.noreply.github.com>
2023-02-14 21:17:40 +0530

    Add files via upload

---
+++ 10.StringPy/main.py
@@ -0,0 +1,12 @@
+string = "abcd"
+# print(string, len(string), type(string))
+
+# ITERATING STRING
+# for i in string:
+# print(i)
+
+# ITERATING USING INDEX VALUE
+
+# for ii in range(-1,len(string)):
+#     print(string[ii])
+# print(string[-1])
