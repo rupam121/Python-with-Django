004a1429547cf48f2bd8dcbe4d4754fffef7d263
RUPAM GIRI <103890234+rupam121@users.noreply.github.com>
2023-02-23 19:44:15 +0530

    Add files via upload

---
+++ 14.2DArrayPy/main.py
@@ -0,0 +1,12 @@
+# TWO DIMENTIONAL ARRAY
+l1 = [[1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]]
+
+# for i in l1:
+#     print(i)
+# print(l1[0][1])#2
+
+for i in range(len(l1)):
+    for j in range(len(l1)):
+        print(l1[i][j])
