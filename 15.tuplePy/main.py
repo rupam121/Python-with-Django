49a361c24b4ab75f199aa30d5b3b8762717e7ff3
RUPAM GIRI <103890234+rupam121@users.noreply.github.com>
2023-02-24 23:33:17 +0530

    Add files via upload

---
+++ 15.tuplePy/main.py
@@ -0,0 +1,7 @@
+# TUPLES ARE USE TO STORE DATA AND IMMUTABLE NOT CHANGE VALUE:)
+
+tuple1 = ("hi", 1, True)
+print(tuple1[1])
+# tuple1[0] = True
+print(tuple1)
+print(tuple1[::-1])  # reverse tuple
